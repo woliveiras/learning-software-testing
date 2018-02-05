a280d0a435003e7282e16d239fef50fba0eb7779
woliveiras <w.oliveira542@gmail.com>
2018-02-05 07:38:26 -0200

    add notes

---
+++ tests/notes.js
@@ -0,0 +1,28 @@
+// describe() serve para testarmos os nossos métodos,
+//   modulos, classes
+// podemos ter vários describes dentro de um mesmo
+//    arquivo
+// ---
+// context() serve para separar o contexto da execução
+//   de um método
+// context serve para separar contextos de testes
+//   para casos de testes de diferentes comportamentos
+//   em um mesmo método
+// o it() é quem realmente vai executar os comandos
+describe('Main', () => {
+  describe('Method A', () => {
+    context('Case 1', () => {
+      it('Should happen blablabla', () => {
+        // esperamos que aconteça algo aqui
+      });
+    });
+
+    context('Case 2', () => {
+
+    });
+  });
+
+  describe('Method B', () => {
+
+  });
+});
