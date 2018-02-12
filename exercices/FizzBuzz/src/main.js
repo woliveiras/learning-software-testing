bef047b636bb2d4bdf745a5bf872b71a75922ec5
woliveiras <w.oliveira542@gmail.com>
2018-02-12 20:22:23 -0200

    Add FizzBuzz exercise

---
+++ exercices/FizzBuzz/src/main.js
@@ -0,0 +1,14 @@
+const FizzBuzz = (num) => {
+  if (num % 3 === 0 && num % 5 === 0) {
+    return 'FizzBuzz';
+  }
+  if (num % 3 === 0) {
+    return 'Fizz';
+  }
+  if (num % 5 === 0) {
+    return 'Buzz';
+  }
+  return num;
+};
+
+export default FizzBuzz;
